0fc64b9465b663323b5386daa901e896dfaae0d6
ctyd <124029849+c-t-y-d@users.noreply.github.com>
2023-03-06 22:26:18 -0500

    Replaced if/elses for a singular switch statement meant to handle the specified operation type read in; declared a separate variable for options as I had to change operation to char for the switch statement to work thus breaking the options that relied on string comparison

--- main.cpp
+++ main.cpp
@@ -6,7 +6,8 @@ Calculator obj;
 int calcNum;
 int num1;
 int num2;
-string operation;
+char operation;
+string options;
 bool run = 1;
 
 int main(){  
@@ -20,39 +21,40 @@ int main(){
             cout << "What kind of operation would you liked performed? a. addition b. subtraction c. multiplication d. division e. square root" << endl;
             cin >> operation;
 
-            if (operation.compare("a") == 0){
-                cout << obj.add(num1, num2) << endl;
-            }
+            switch(operation){
+                case 'a':
+                    cout << obj.add(num1, num2) << endl;
+                    break;
 
-            else if (operation.compare("b") == 0){
-                cout << obj.sub(num1, num2) << endl;
+                case 'b':
+                    cout << obj.sub(num1, num2) << endl;
+                    break;
+                case 'c':
+                    cout << obj.mult(num1, num2) << endl;
+                    break;
+                case 'd':
+                    cout << obj.div(num1, num2) << endl;
+                    break;
+                case 'e':
+                    cout << "root 1: " << obj.root(num1) << endl << "root 2: " << obj.root(num2) << endl;
+                    break;
+                default:
+                    cout << "Invalid option" << endl;
+                    break;
             }
-
-            else if (operation.compare("c") == 0){
-                cout << obj.mult(num1, num2) << endl;
-            }
-
-            else if (operation.compare("d") == 0){
-                cout << obj.div(num1, num2) << endl;
-            }
-
-            else if (operation.compare("e") == 0){
-                cout << "root 1: " << obj.root(num1) << endl << "root 2: " << obj.root(num2) << endl;
-            }
-
             calcNum++;
             cout << "Total calculations this session: " << calcNum << "\nUser Options [O]\n" << "\nWould you like to calculate anything else (Y/N)?" << endl;
-            cin >> operation;
-
-            if (operation.compare("n") == 0){
+            cin >> options;
+  
+            if (options.compare("n") == 0||options.compare("N") == 0){
                 run = 0;
             }
 
-            else if (operation.compare("O") == 0){
+            else if (options.compare("O") == 0){
                 cout << "   s\t Set the calculation count\n";
                 cout << "   l\t Exit the calculator\n";
                 cin >> operation;
-                if (operation.compare("s") == 0){
+                if (options.compare("s") == 0){
                     cout << "What would you like the new count to be? ";
                     int newCount; 
                     cin >> newCount;
@@ -60,7 +62,7 @@ int main(){
                     calcNum = newCount;
                     cout << "New count: " << obj.getCalcAmount() << endl;
                 }
-                if (operation.compare("l") == 0){
+                if (options.compare("l") == 0){
                     break;
                 }
                 cout << "Would you like to calculate anything else (Y/N)?" << endl;
